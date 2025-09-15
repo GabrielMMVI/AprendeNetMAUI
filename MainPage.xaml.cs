f57e8771e2edc4c314107c7aac5e465a26dc2580
Gabriel Junqueira <161396206+GabrielMMVI@users.noreply.github.com>
2025-09-15 08:34:30 -0300

    App calcula gasolina criado

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -12,6 +12,30 @@
         private void Button_Clicked(object sender, EventArgs e)
         {
 
+            try
+            {
+                double etanol = Convert.ToDouble(txt_etanol.Text);
+                double gasolina = Convert.ToDouble(txt_gasolina.Text);
+
+                string msg = "";
+
+                if (etanol <= (gasolina * 0.7))
+                {
+                    msg = "Abasteça com Etanol!";
+                }
+                else
+                {
+                    msg = "Abasteça com Gasolina!";
+                }
+              
+                DisplayAlert("Resultado", msg, "Fechar");
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Ops! Ocorreu um erro!", ex.Message, "Fechar");
+                throw;
+            }
+
         }
     }
 
